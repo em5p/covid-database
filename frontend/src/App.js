086d52b36865695bf88e1a01e0ac821ca3ffde71
Vivekhaz <vivekdhazari@gmail.com>
2020-11-21 20:45:57 -0500

    using grommet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 
-function App() {
+const App = () => {
+
+  // App State
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar> 
+          <Heading level='3' margin='none'>COVID Dashboard</Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => setShowSidebar(!showSidebar)}/>
+        </AppBar>
+        {/* Main Body */}
+        
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+          </Box>
+          <Collapsible direction="horizontal">
+            <Box
+              flex
+              width='medium'
+              background='light-2'
+              elevation='small'
+              align='center'
+              justify='center'>
+              sidebar
+            </Box>
+          </Collapsible>
+          
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
