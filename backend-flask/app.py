409005d6e3916d019ce435418a876294f3acbfd8
Vivekhaz <vivekdhazari@gmail.com>
2020-11-21 21:26:06 -0500

    requirements added

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,11 +1,33 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+# Localhost:5000/
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/api/agg_test_result', methods=['GET', 'POST'])
-def aggregate_test_result():
+# localhost:5000/api/agg_test_result : GET
+@app.route('/api/agg_test_results')
+def agg_test_results():
+  """
+  Arguments:
+    Location:
+    Housing:
+    Testing Site:
+    Date Processed Start, End
+
+    {'location': location, 'housing': housing .....}
+  """
+
+  if request:
+
+    location = request['location']
+    # ...
+
+    # Plug that into mysql
+    # Dictionary of tuples
+    # {total : (number, percent), positive : (number, percent)}
+    return_json = jsonify()
+    return return_json
   pass
