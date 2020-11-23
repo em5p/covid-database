1a297fb317038d0d39e59ecc20c236a388f7e015
Goh, Ema <egoh9@gatech.edu>
2020-11-23 18:35:40 -0500

    explore results page

--- frontend/src/ExploreTestResult.js
+++ frontend/src/ExploreTestResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+    DataTable,
     Grommet,
     Table,
     TableBody,
@@ -18,9 +19,7 @@ import {
 class ExploreTestResult extends React.Component {
     constructor(props) {
         super(props);
-        this.onPageChange = this.onPageChange.bind(this);
-        this.onLogin = this.onLogin.bind(this);
-        
+        this.onPageChange = this.onPageChange.bind(this);  
     }
     onPageChange() {
         this.props.onPageChange('Home Page');
@@ -59,40 +58,69 @@ class ExploreTestResult extends React.Component {
               },
             },
           };
+          const rows = [
+            { property: 'testID', header: <Text>Test ID #</Text>,},
+            { property: 'date_tested', header: <Text>Date Tested</Text>,},
+            { property: 'timeslot', header: <Text>Timeslot</Text>,},
+            { property: 'test_location', header: <Text>Testing Location</Text>,},
+            { property: 'date_processed', header: <Text>Date Processed</Text>,},
+            { property: 'pooled_result', header: <Text>Pooled Result</Text>,},
+            { property: 'individual_result', header: <Text>Individual Result</Text>,},
+            { property: 'processed_by', header: <Text>Processed By</Text>,}];
+
+          const sample_results = [{testID: 1}, {date_tested: '8/19/20'}, {timeslot: '2:00 PM'}, 
+            {test_location: 'West Campus'}, {date_processed: '8/20/20'},
+            {pooled_result: 'Positive'}, {individual_result: 'Negative'}, 
+            {processed_by: 'Bob Waters'}]
+          const sample_data = [
+            { col1: 'Test ID #', col2: 1},
+            { col1: 'Date Tested', col2: '8/19/20'},
+            { col1: 'Timeslot', col2: '2:00 PM'},
+            { col1: 'Testing Location', col2: 'West Campus'},
+            { col1: 'Date Processed', col2: '8/20/20'},
+            { col1: 'Pooled Result', col2: 'Positive'},
+            { col1: 'Individual Result', col2: 'Negative'}, 
+            { col1: 'Processed By', col2: 'Bob Waters'}
+          ]
+
+          const columns = [
+            { property: 'col1'}, { property: 'col2'}
+          ]
+    
+                        
         
         return (
             <Grommet theme={customTheme}>
             <Box align="center" pad="large">
-              <Table caption="Custom Theme Table">
-                <TableHeader>
-                  <TableRow>
+              <Text weight='bold'>Explore Test Results</Text>
+              {/* <Table caption="Explore Test Results"> */}
+              <Table>
+                <TableBody>
+
+                {sample_data.map(datum => (
+                  <TableRow key={datum.id}>
                     {columns.map(c => (
-                      <TableCell key={c.property} scope="col" align={c.align}>
-                        <Text>{c.label}</Text>
+                      <TableCell key={c.property} scope={c.dataScope} align={c.align}>
+                        <Text>{c.format ? c.format(datum) : datum[c.property]}</Text>
                       </TableCell>
                     ))}
                   </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {data.map(datum => (
+                ))}
+
+                  {/* {sample_results.map(datum => (
                     <TableRow key={datum.id}>
-                      {columns.map(c => (
-                        <TableCell key={c.property} scope={c.dataScope} align={c.align}>
-                          <Text>{c.format ? c.format(datum) : datum[c.property]}</Text>
+                      <Text>hi</Text>
+                      {rows.map(row => (
+                        <TableCell key={row.property} scope={row.dataScope} align={row.align}>
+                          <Text>{sample_results[{row}]}</Text>
+                          <Text>{row.property}</Text>
+                          <Text>{row.format ? row.format(datum) : datum[row.property]}</Text>
                         </TableCell>
                       ))}
                     </TableRow>
-                  ))}
+                  ))} */}
+                  
                 </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    {columns.map(c => (
-                      <TableCell key={c.property} align={c.align}>
-                        <Text>{c.footer}</Text>
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableFooter>
               </Table>
             </Box>
           </Grommet>
